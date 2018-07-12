ec41f32ba95c5e0f13c27fc1da13a8ef410a95fd
mister4L1F <41142326+mister4L1F@users.noreply.github.com>
2018-07-12 14:57:21 +0700

    Add files via upload

---
+++ BPH.sh
@@ -0,0 +1,282 @@
+#!/system/xbin/bash
+#Hacking Tools
+#Hacking Is My Live
+#Author Mr.4L1F
+#Lu Mau Recode Ya Njeng,Izin Ke Gw Dulu
+clear
+cd module
+blue="\033[34;1m"
+green="\033[32;1m"
+purple="\033[35;1m"
+cyan="\033[36;1m"
+red="\033[31;1m"
+white="\033[37;1m"
+yellow="\033[33;1m"
+sleep 1
+echo $red
+echo ""
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo "\033[32;1mAuthor : Mr.4L1F"
+echo "\033[35;1mTeam : Black PHINIX Hacker"
+echo "\033[33;1mKontak Me : 08981380315"
+echo "\033[31;1mPlease Follow Ig Me : Mr.4L1F"
+echo "\033[92mBlack PHINIX Hacker"
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[===============================================]"
+echo $green "1.> Drupal RCE"
+echo "[===============================================]"
+echo $purple "2.> Admin Finder"
+echo "[===============================================]"
+echo $cyan "3.> Upload Admin Finder"
+echo "[===============================================]"
+echo $red "4.> Red_Hawk"
+echo "[===============================================]"
+echo $white "5.> Ngrok"
+echo "[===============================================]"
+echo $yellow "6.> Lokomedia Exploiter"
+echo "[===============================================]"
+echo $green "7.> Ko-Dork"
+echo "[===============================================]"
+echo $purple "8.> Scan-Website-Admin-Login"
+echo "[===============================================]"
+echo $cyan "9.> Cari Panel Login Admin Pakai Breacher"
+echo "[===============================================]"
+echo $red "10.> Google Dork"
+echo "[===============================================]"
+echo $white "11.> Remote Shell"
+echo "[===============================================]"
+echo $yellow "12.> Host Checker"
+echo "[===============================================]"
+echo $green "13.> LazySQL Map"
+echo "[===============================================]"
+echo $purple "14.> Elfinder Exploiter"
+echo "[===============================================]"
+echo $cyan "15.> Microsploit"
+echo "[===============================================]"
+echo $red "0.> Keluar"
+echo "[===============================================]"
+echo "\033[32;1m"
+read -p "root@TOOLsPHINIX~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+sleep 1
+pkg install wget
+wget https://pastebin.com/raw/M0cCyaB7 -O rce.sh
+chmod +x rce.sh
+mv rce.sh $HOME
+./rce.sh
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg install git
+pkg install php
+git clone https://github.com/mrcakil/admin_finder
+mv admin_finder $HOME
+cd $HOME/admin_finder
+python2 gam.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+toilet "Mr.4L1F"
+python2 upf.py
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg install git
+pkg install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+mv RED_HAWK $HOME
+cd $HOME/RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+apt-get install wget
+mkdir ngrok
+cd ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+echo $red "Done Install Tools"
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F
+pkg install wget
+wget https://pastebin.com/raw/sPpJRjCZ -O lokomedia.php
+mv lokomed $HOME
+cd $HOME/lokomed
+php lokomedia.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg install git
+pkg install python2
+pkg install git
+git clone https://github.com/CiKu370/ko-dork
+mv ko-dork $HOME
+chmod +x ko-dork.py
+python2 dork.py
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg install git
+git clone https://github.com/DarknessCyberTeam/Scan-Website-Admin-Login
+mv Scan-Website-Admin-Login $HOME
+cd $HOME/Scan-Website-Admin-Login
+python2 Admin-Login.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg install python2
+pkg install git
+git clone https://github.com/UltimateHackers/Breacher.git
+mv Breacher $HOME
+cd $HOME/Breacher
+python2 breacher.py
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/XG77Z10/Google-Dork
+mv Google-Dork $HOME
+cd $HOME/Google-Dork
+python2 Google.py
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+pkg update
+pkg upgrade
+pkg install git
+apt install perl
+git clone https://github.com/aryanrtm/Remote-Shell
+mv Remote-Shell $HOME
+cd $HOME/Remote-Shell
+perl remoteshell.pl
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/pirmansx/hostchecker
+mv hostchecker $HOME
+cd $HOME/hostchecker
+python2 HC.py
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+sleep 1
+pkg update
+pkg upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/verluchie/termux-lazysqlmap
+mv termux-lazysqlmap $HOME
+cd $HOME/lazysqlmap
+python2 lazysqlmap.py
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+sleep 1
+apt-get install wget
+pkg install php
+mkdir elfinder
+cd elfinder
+wget https://pastebin.com/raw/S7Y2V19h -O elfinder.php
+mv elfinder $HOME
+cd $HOME/elfinder
+php elfinder.php
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+toilet -f big -F gay "Mr.4L1F"
+sleep 1
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/Screetsec/Microsploit
+mv Microsploit $HOME
+chmod 777 Microsploit
+./Microsploit
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mBlack PHINIX Hacker"
+sleep 1
+echo "\033[33;1mDear Black PHINIX Hacker"
+sleep 1
+echo "Dimana Keadilan"
+sleep 1
+echo "Yang Korupsi Di Bebaskan"
+sleep 1
+echo "Karena Ada Uang"
+sleep 1
+echo "Yang Cuma Menguji Keamanan Website"
+sleep 1
+echo "Di Penjara"
+sleep 1
+echo "Kami Tidak Akan Menjadi Baik"
+sleep 1
+echo "Kami Akan Menjadi Jahat"
+sleep 1
+echo "Sebelum Para Korupsi Belum Di Penjara"
+sleep 1
+echo "Kami Akan Selalu Menjadi Jahat"
+sleep 1
+echo "Save Mr.4L1F"
+sleep 1
+echo "\033[32;1mTeam Black PHINIX Hacker"
+sleep 1
+exit
+fi
